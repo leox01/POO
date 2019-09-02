247c1d9a229696bf3cf9c2ac818478920de58a1f
Leopoldo Morales <leo01morales@gmail.com>
2019-09-02 11:35:28 -0500

    Juego del Gato en Java completo :)

--- src/juego/del/gato/JuegoDelGato.java
+++ src/juego/del/gato/JuegoDelGato.java
@@ -10,6 +10,9 @@ public class JuegoDelGato {
     public static char array[][] = new char[3][3];
     public static char jugador = 'X';
     
+    public static boolean hayGanador;
+    public static boolean espaciosDisponibles;
+    public static int respuesta;
     public static void cambiarTurno(){
     
         if(jugador=='X'){
@@ -50,144 +53,188 @@ public class JuegoDelGato {
     public static void main(String[] args) {
         
         Scanner teclado = new Scanner(System.in);  
+        Scanner tecladoInt = new Scanner(System.in);  
         char casilla;
         
         boolean repetirTurnoMismoJugador = false;
-       
-        //llenar el arreglo
-        llenarArreglo();
-        
-        do{ // do-while para cambiar de turno
-            do{ //do-while para repetir turno del mismo jugador
-                //imprimir arreglo
-                imprimirArreglo();
-                //pedir la casilla al jugador 1
-                System.out.println("Dame la letra donde quieras escribir la "+jugador);
-                casilla = teclado.nextLine().charAt(0);
+        do{
+            //llenar el arreglo
+            llenarArreglo();
 
-                switch(casilla){
+            do{ // do-while para cambiar de turno
+                do{ //do-while para repetir turno del mismo jugador
+                    //imprimir arreglo
+                    imprimirArreglo();
+                    //pedir la casilla al jugador 1
+                    System.out.println("Dame la letra donde quieras escribir la "+jugador);
+                    casilla = teclado.nextLine().charAt(0);
 
-                    case 'A':
-                    case 'a':
-                        if(array[0][0]=='A'){
-                            array[0][0] = jugador;
-                            repetirTurnoMismoJugador = false;
-                        }else{
-                            System.out.println("Error la casilla ya está ocupada");
-                            repetirTurnoMismoJugador = true;
-                        }
-                        break;
-                    case 'B':
-                    case 'b':
-                        if(array[0][1]=='B'){
-                            array[0][1] = jugador;
-                            repetirTurnoMismoJugador = false;
-                        }else{
-                            System.out.println("Error la casilla ya está ocupada");
-                            repetirTurnoMismoJugador= true;
-                        }
-                        break;
-                    case 'C':
-                    case 'c':
-                        if(array[0][2]=='C'){
-                            array[0][2] = jugador;
-                            repetirTurnoMismoJugador = false;
-                        }else{
-                            System.out.println("Error la casilla ya está ocupada");
-                            repetirTurnoMismoJugador= true;
-                        }
-                        break;
-                    case 'D':
-                    case 'd':
-                        if(array[1][0]=='D'){
-                            array[1][0] = jugador;
-                            repetirTurnoMismoJugador = false;
-                        }else{
-                            System.out.println("Error la casilla ya está ocupada");
-                            repetirTurnoMismoJugador= true;
-                        }
-                        break;
-                    case 'E':
-                    case 'e':
-                        if(array[1][1]=='E'){
-                            array[1][1] = jugador;
-                            repetirTurnoMismoJugador = false;
-                        }else{
-                            System.out.println("Error la casilla ya está ocupada");
-                            repetirTurnoMismoJugador= true;
-                        }
-                        break;
-                    case 'F':
-                    case 'f':
-                        if(array[1][2]=='F'){
-                            array[1][2] = jugador;
-                            repetirTurnoMismoJugador = false;
-                        }else{
-                            System.out.println("Error la casilla ya está ocupada");
-                            repetirTurnoMismoJugador= true;
-                        }
-                        break;
-                    case 'G':
-                    case 'g':
-                        if(array[2][0]=='G'){
-                            array[2][0] = jugador;
-                            repetirTurnoMismoJugador = false;
-                        }else{
-                            System.out.println("Error la casilla ya está ocupada");
-                            repetirTurnoMismoJugador= true;
-                        }
-                        break;
-                    case 'H':
-                    case 'h':
-                        if(array[2][1]=='H'){
-                            array[2][1] = jugador;
-                            repetirTurnoMismoJugador = false;
-                        }else{
-                            System.out.println("Error la casilla ya está ocupada");
-                            repetirTurnoMismoJugador= true;
-                        }
-                        break;
-                    case 'I':
-                    case 'i':
-                        if(array[2][2]=='I'){
-                            array[2][2] = jugador;
-                            repetirTurnoMismoJugador = false;
-                        }else{
-                            System.out.println("Error la casilla ya está ocupada");
-                            repetirTurnoMismoJugador= true;
-                        }
-                        break;
+                    switch(casilla){
 
-                    default: System.out.println("No existe esa casilla ");
-                             repetirTurnoMismoJugador = true;
-                }
+                        case 'A':
+                        case 'a':
+                            if(array[0][0]=='A'){
+                                array[0][0] = jugador;
+                                repetirTurnoMismoJugador = false;
+                            }else{
+                                System.out.println("Error la casilla ya está ocupada");
+                                repetirTurnoMismoJugador = true;
+                            }
+                            break;
+                        case 'B':
+                        case 'b':
+                            if(array[0][1]=='B'){
+                                array[0][1] = jugador;
+                                repetirTurnoMismoJugador = false;
+                            }else{
+                                System.out.println("Error la casilla ya está ocupada");
+                                repetirTurnoMismoJugador= true;
+                            }
+                            break;
+                        case 'C':
+                        case 'c':
+                            if(array[0][2]=='C'){
+                                array[0][2] = jugador;
+                                repetirTurnoMismoJugador = false;
+                            }else{
+                                System.out.println("Error la casilla ya está ocupada");
+                                repetirTurnoMismoJugador= true;
+                            }
+                            break;
+                        case 'D':
+                        case 'd':
+                            if(array[1][0]=='D'){
+                                array[1][0] = jugador;
+                                repetirTurnoMismoJugador = false;
+                            }else{
+                                System.out.println("Error la casilla ya está ocupada");
+                                repetirTurnoMismoJugador= true;
+                            }
+                            break;
+                        case 'E':
+                        case 'e':
+                            if(array[1][1]=='E'){
+                                array[1][1] = jugador;
+                                repetirTurnoMismoJugador = false;
+                            }else{
+                                System.out.println("Error la casilla ya está ocupada");
+                                repetirTurnoMismoJugador= true;
+                            }
+                            break;
+                        case 'F':
+                        case 'f':
+                            if(array[1][2]=='F'){
+                                array[1][2] = jugador;
+                                repetirTurnoMismoJugador = false;
+                            }else{
+                                System.out.println("Error la casilla ya está ocupada");
+                                repetirTurnoMismoJugador= true;
+                            }
+                            break;
+                        case 'G':
+                        case 'g':
+                            if(array[2][0]=='G'){
+                                array[2][0] = jugador;
+                                repetirTurnoMismoJugador = false;
+                            }else{
+                                System.out.println("Error la casilla ya está ocupada");
+                                repetirTurnoMismoJugador= true;
+                            }
+                            break;
+                        case 'H':
+                        case 'h':
+                            if(array[2][1]=='H'){
+                                array[2][1] = jugador;
+                                repetirTurnoMismoJugador = false;
+                            }else{
+                                System.out.println("Error la casilla ya está ocupada");
+                                repetirTurnoMismoJugador= true;
+                            }
+                            break;
+                        case 'I':
+                        case 'i':
+                            if(array[2][2]=='I'){
+                                array[2][2] = jugador;
+                                repetirTurnoMismoJugador = false;
+                            }else{
+                                System.out.println("Error la casilla ya está ocupada");
+                                repetirTurnoMismoJugador= true;
+                            }
+                            break;
 
-            }while(repetirTurnoMismoJugador);
+                        default: System.out.println("No existe esa casilla ");
+                                 repetirTurnoMismoJugador = true;
+                    }
 
-            //verificar si hay ganador
-            boolean seguirJugando = verificarGanador(jugador);
+                }while(repetirTurnoMismoJugador);
 
-            //verificar si hay empate
-            verificarEmpate();
-             
-            //cambiar el turno del jugaro
-            cambiarTurno();
+                //verificar si hay ganador
+               hayGanador = verificarGanador(jugador);
+
+                //verificar si hay empate
+               espaciosDisponibles = verificarEspaciosDisponibles();
+
+
+               if(hayGanador)
+                   System.out.println("Felicidades el jugador "+jugador+" es el ganador");
+
+
+               if(!espaciosDisponibles)
+                   System.out.println("Hay un empate!!!");
+
+               if(espaciosDisponibles)
+                   //cambiar el turno del jugaro
+                    cambiarTurno();
+
+            }while(!hayGanador&&espaciosDisponibles); 
+
+            System.out.println("¿Quieres volver a jugar?1.-Si, 2.-No");
+
+            respuesta = tecladoInt.nextInt();
             
-        }while(true); 
+        
+        
+        }while(respuesta==1);
+        
        
         
     }
 
     public static boolean verificarGanador(char jugador) {
-        System.out.println("Verificando ganador....");
-    
         
-            
-        return true;
+        
+        return (array[0][0]==jugador&&array[0][1]==jugador&&array[0][2]==jugador||
+                array[1][0]==jugador&&array[1][1]==jugador&&array[1][2]==jugador|| 
+                array[2][0]==jugador&&array[2][1]==jugador&&array[2][2]==jugador||
+                array[0][0]==jugador&&array[1][0]==jugador&&array[2][0]==jugador||
+                array[0][1]==jugador&&array[1][1]==jugador&&array[2][1]==jugador||
+                array[0][2]==jugador&&array[1][2]==jugador&&array[2][2]==jugador||
+                array[0][0]==jugador&&array[1][1]==jugador&&array[2][2]==jugador||
+                array[0][2]==jugador&&array[1][1]==jugador&&array[2][0]==jugador);
+    
+ 
     }
 
-    public static void verificarEmpate() {
-         System.out.println("Verificando empate....");
+    public static boolean verificarEspaciosDisponibles() {
+        
+        for (int filas = 0; filas < 3; filas++) {
+            for (int columnas = 0; columnas < 3; columnas++) {
+                if (array[filas][columnas]=='A'||
+                    array[filas][columnas]=='B'|| 
+                    array[filas][columnas]=='C'||
+                    array[filas][columnas]=='D'|| 
+                    array[filas][columnas]=='E'||
+                    array[filas][columnas]=='F'|| 
+                    array[filas][columnas]=='G'||
+                    array[filas][columnas]=='H'|| 
+                    array[filas][columnas]=='I'){
+                            return true;
+                }
+            }
+            
+        }
+        
+        return false;
     }
     
 }
